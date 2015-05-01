d56d05726de4b9a07032bba53272bbba103df587
cmmisura14 <cmmisura14@gmail.com>
2015-05-01 15:31:51 -0500

    Add jquery

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 /**
  * Created by codymisura on 5/1/15.
  */
+$(document).ready(function(){
+    $.ajax({
+        type: 'GET',
+        url: 'https://api.github.com/users/cmmisura14/orgs/repos',
+        crossDomain: true,
+        Accept: "application/vnd.github.v3+json",
+        dataType: 'text',
+        success: function (response) {
+            console.log(response);
+        },
+        error: function (xhr, status) {
+            alert('Error: ' + status);
+        }
+    });
+});
