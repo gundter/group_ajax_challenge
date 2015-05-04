5bb5c180ea065f97b8ca1b71e3e2cf9f310eaca5
Terry Gunderson <tgunderson25@gmail.com>
2015-05-04 12:02:31 -0500

    Implements search functionality

--- script.js
+++ script.js
@@ -2,10 +2,11 @@
  * Created by codymisura on 5/1/15.
  */
 
-function getUser (){
+function getUser (query){
+    console.log(query);
     $.ajax({
         type: 'GET',
-        url: 'https://api.github.com/users/tgun6144?client_id=f8a4b95805c9804c9eb7&client_secret=4b1bff35a5b8b802fe4bb4e1204afd2f56fc8d8d',
+        url: 'https://api.github.com/users/'+query+'?client_id=f8a4b95805c9804c9eb7&client_secret=4b1bff35a5b8b802fe4bb4e1204afd2f56fc8d8d',
         crossDomain: true,
         //accept: "application/vnd.github.v3+json",
         dataType: 'json',
@@ -22,11 +23,23 @@ function getUser (){
 
 function displayInfo (data){
     // username (login)
-    // link to github page (url)
+    // link to github page (html_url)
     // profile image (avatar_url)
     // some public info? (name, company, location, bio)
+
+    var avatar_url = data.avatar_url.toString();
+    console.log(avatar_url);
+    $('.results').find("img").attr("src", avatar_url);
+    $('.results').append("<a href='"+data.html_url+"'>"+data.login+"</a> <p><strong>"+data.name+"</strong>, "+data.company+", "+data.location+"</p><p>"+data.bio+"</p>");
+
 }
 
 $(document).ready(function(){
-    getUser();
+    // Start the search here!
+    $('.controls').on('click', '.searchBtn', function(){
+        var searchTerm = $(".controls").find("#searchField").val();
+        console.log(searchTerm);
+        $(".controls").find("#searchField").val('');
+        getUser(encodeURI(searchTerm));
+    });
 });
