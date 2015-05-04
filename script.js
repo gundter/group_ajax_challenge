8820d8b08b584a1e5715d674a3112d2df168983c
Terry Gunderson <tgunderson25@gmail.com>
2015-05-04 11:09:42 -0500

    first commit

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
 /**
  * Created by codymisura on 5/1/15.
  */
-$(document).ready(function(){
+
+function getUser (){
     $.ajax({
         type: 'GET',
-        url: 'https://api.github.com/users/cmmisura14/orgs/repos',
+        url: 'https://api.github.com/users/tgun6144?client_id=f8a4b95805c9804c9eb7&client_secret=4b1bff35a5b8b802fe4bb4e1204afd2f56fc8d8d',
         crossDomain: true,
-        Accept: "application/vnd.github.v3+json",
-        dataType: 'text',
+        //accept: "application/vnd.github.v3+json",
+        dataType: 'json',
         success: function (response) {
             console.log(response);
+            displayInfo(response);
+            return response;
         },
         error: function (xhr, status) {
             alert('Error: ' + status);
         }
     });
+}
+
+function displayInfo (data){
+    // username (login)
+    // link to github page (url)
+    // profile image (avatar_url)
+    // some public info? (name, company, location, bio)
+}
+
+$(document).ready(function(){
+    getUser();
 });
